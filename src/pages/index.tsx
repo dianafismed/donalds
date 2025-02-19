8e7a0e69039047bae43d99042f7a558a2f3422c7
Diana Avila <missernightangel@gmail.com>
2025-02-18 22:45:42 -0300

    up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
-const Index = () => {
-  return (
-    <div>
-      <Button>Botao</Button>
-      <Input placeholder="Input"></Input>
-    </div>
-  );
-};
-
-export default Index;
+export default function Home() {
+  return <div></div>;
+}
---
+++ src/pages/product.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>Botao</Button>
+      <Input placeholder="Input"></Input>
+    </div>
+  );
+};
+
+export default HomePage;
