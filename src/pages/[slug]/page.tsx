fc76758799dbcc3f5657b00aa0562daceef92fcf
Diana Avila <missernightangel@gmail.com>
2025-02-19 18:56:47 -0300

    up

--- src/pages/[slug]/page.tsx
+++ src/pages/[slug]/page.tsx
@@ -1,9 +1,12 @@
+import { db } from "@/lib/prisma";
+
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
 }
 
 const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   const { slug } = await params;
+  await db.restaurant.findFirst({ where: { slug: slug } });
   return <h1>{slug}</h1>;
 };
 
